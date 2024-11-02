b0de6a30b2cf3c9de2b95f523c12ae1c59ec2d4d
Higor Tavares <paulohtfreire@gmail.com>
2024-11-01 23:41:53 -0300

    system design

--- apigw.tf
+++ apigw.tf
@@ -12,7 +12,8 @@ resource "aws_api_gateway_method" "apigw-method" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.apigw-resource.id
   http_method = "POST"
-  authorization = "NONE"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.auth.id
 }
 
 resource "aws_api_gateway_integration" "lambda_integration" {
@@ -29,8 +30,6 @@ resource "aws_lambda_permission" "apigw_lambda" {
   action        = "lambda:InvokeFunction"
   function_name = module.lambda_function.lambda_function_name
   principal     = "apigateway.amazonaws.com"
-
-  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
   source_arn = "arn:aws:execute-api:us-west-1:418272752891:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.apigw-method.http_method}${aws_api_gateway_resource.apigw-resource.path}"
 }
 resource "aws_api_gateway_deployment" "deployment" {
@@ -43,10 +42,18 @@ resource "aws_api_gateway_deployment" "deployment" {
   lifecycle {
     create_before_destroy = true
   }
+  depends_on = [ aws_api_gateway_method.apigw-method ]
 }
 
-resource "aws_api_gateway_stage" "example" {
+resource "aws_api_gateway_stage" "development" {
   deployment_id = aws_api_gateway_deployment.deployment.id
   rest_api_id   = aws_api_gateway_rest_api.api.id
   stage_name    = "development"
+}
+resource "aws_api_gateway_authorizer" "auth" {
+  name = "authgateway"
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  type          = "COGNITO_USER_POOLS"
+  provider_arns = [aws_cognito_user_pool.pool.arn]
+  identity_source = "method.request.header.Authorization"
 }
---
+++ cognito.tf
@@ -0,0 +1,40 @@
+resource "aws_cognito_user_pool" "pool" {
+  name = "payments_webhook"
+}
+
+resource "aws_cognito_user_pool_client" "app_client" {
+  name = "postman-client-app"
+
+  user_pool_id = aws_cognito_user_pool.pool.id
+  token_validity_units {
+    refresh_token = "days"
+    access_token  = "minutes"
+    id_token      = "hours"
+  }
+  refresh_token_validity = 1
+  access_token_validity  = 60
+  id_token_validity      = 1
+  generate_secret = true
+  prevent_user_existence_errors = "ENABLED"
+  enable_token_revocation = true
+  allowed_oauth_flows = ["client_credentials"]
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_scopes = ["webhook/write"]
+}
+
+resource "aws_cognito_user_pool_domain" "main" {
+  domain       = "dd5dcbea-a19a-4e6f-9ab2-d9fbdbdeb516"
+  user_pool_id = aws_cognito_user_pool.pool.id
+}
+
+resource "aws_cognito_resource_server" "resource" {
+  identifier = "webhook"
+  name       = "webhook"
+
+  scope {
+    scope_name        = "write"
+    scope_description = "Write messages"
+  }
+
+  user_pool_id = aws_cognito_user_pool.pool.id
+}
